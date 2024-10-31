558f8c8c9ea6652f6b9fa8b2cb7fa72bf9636336
Omar Alkadri <omar.omar.alkadri111@gmail.com>
2024-10-31 21:45:02 +0300

    fix build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './App.css'
-import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 
 
